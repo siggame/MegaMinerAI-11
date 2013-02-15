3e3c2192c8e8ef66d663f029024a83f2a9efbe76
Justin Kellogg <jdk998@mst.edu>
2013-02-15 17:21:58 -0600

    Code Sprint 1 - First draft of data.py

--- codegen/data.py
+++ codegen/data.py
@@ -3,52 +3,125 @@ from structures import *
 
 aspects = ['timer']
 
-gameName = "chess"
+gameName = "Reef"
 
 constants = [
   ]
 
 playerData = [
+  Variable('curReefHealth', int, 'The player\'s current reef health'),
+  Variable('sandDollars', int, 'Currency for fish'),
   ]
 
 playerFunctions = [
-  ]
+  Function('talk', [Variable('message', str)], doc='Allows a player to display messages on the screen'),
+]
 
-Piece = Model('Piece',
-  data=[ Variable('owner', int, 'The owner of the piece'),
-    Variable('file', int, 'The letter this piece is at (1-8)'),
-    Variable('rank', int, 'The number this piece is at (1-8)'),
-    Variable('hasMoved', int, '1=has moved, 0=has not moved'),
-    Variable('type', int, 'The letter that describes this piece\'s type. K=King, Q=Queen, B=Bishop, N=Knight, R=Rook, P=Pawn'),
-    ],
-  doc='A chess piece',
-  functions=[ Function('move', [Variable('file', int), Variable('rank', int), Variable('type', int) ]) ],
-  )
-
-Move = Model('Move',
-  data=[ Variable('fromFile', int, 'The initial file location'),
-    Variable('fromRank', int, 'The initial rank location'),
-    Variable('toFile', int, 'The final file location'),
-    Variable('toRank', int, 'The final rank location'),
-    Variable('promoteType', int, 'The type of the piece for pawn promotion. Q=Queen, B=Bishop, N=Knight, R=Rook'),
-    ],
-  doc='A chess move',
-  )
-
-move = Animation('move',
-  data=[ Variable('fromFile', int),
-    Variable('fromRank', int),
-    Variable('toFile', int),
-    Variable('toRank', int),
-    Variable('promoteType', int),
-    ],
-  )
+Mappable = Model('Mappable',
+  data=[
+    Variable('x', int, 'X position of the object'),
+    Variable('y', int, 'Y position of the object'),
+  ],
+  doc='A mappable object!',
+)
 
 globals = [
+  Variable('dollarsPerTurn', int, 'How many sand dollars a player receives'),
   Variable('turnNumber', int, 'How many turns it has been since the beginning of the game'),
   Variable('playerID', int, 'Player Number; either 0 or 1'),
   Variable('gameNumber', int, 'What number game this is for the server'),
-  Variable('TurnsToStalemate', int, 'How many turns until the game ends because no pawn has moved and no piece has been taken'),
+  Variable('turnsTillSpawn',int, 'Turns until you can spawn new fish'),
+  Variable('maxReefHealth',int,'How much health a reef has initially'),
+  Variable('trashDamage',int,'How much damage trash does'),
+  Variable('mapWidth', int, 'How wide the map is'),
+  Variable('mapHeight', int, 'How high the map is'),
   ]
+
+Trash = Model('Trash',
+  parent=Mappable,
+  data=[ Variable('weight', int, 'The weight of the trash')],
+  doc='This is a Trash object',
+)
+
+Fish = Model('Fish',
+  parent=Mappable,
+  data=[ Variable('owner', int, 'The owner of this fish'),
+    Variable('species', str, 'The type/species of the fish'),
+    Variable('maxHealth', int, 'The maximum health of the fish'),
+    Variable('curHealth', int, 'The current health of the fish'),
+    Variable('maxMoves', int, 'The maximum number of movements in a turn'),
+    Variable('movementLeft', int, 'The number of movements left'),
+    Variable('carryCap', int, 'The total weight the fish can carry'),
+    Variable('carryWeight', int, 'The current amount of weight the fish is carrying'),
+    Variable('attackPower', int, 'The power of the fish\'s attack'),
+    Variable('isVisible', int, 'The visibleness of the fish'),
+    Variable('attacksLeft', int, 'The number of attacks a fish has left'),
+    ],
+  functions=[
+    Function('move', [Variable('x', int), Variable('y', int)], 
+    doc='Command a fish to move to a specified position'),
+    Function('pickUp', [Variable('x', int), Variable('y', int), Variable('weight', int)],
+    doc='Command a fish to pick up some trash at a specified position'),
+    Function('drop', [Variable('x', int), Variable('y', int), Variable('weight', int)],
+    doc='Command a fish to drop some trash at a specified position'),
+    Function('attack', [Variable('x', int), Variable('y', int)],
+    doc='Command a fish to attack another fish at a specified position'),
+   ]
+  )
+
+move = Animation('move',
+  data=[
+    Variable('actingID', int),
+    Variable('fromX', int),
+    Variable('fromY', int),
+    Variable('toX', int),
+    Variable('toY', int),
+  ],
+  )
+  
+playerTalk = Animation('playerTalk',
+  data=[
+    Variable('actingID', int),
+    Variable('message', str),
+  ],
+)
+
+attack = Animation('attack',
+  data=[
+    Variable('actingID', int),
+    Variable('targetID', int),
+  ],
+)
+
+spawn = Animation('spawn',
+  data=[
+    Variable('x', int),
+    Variable('y', int),
+  ],
+)
+
+pickUp = Animation('pickUp',
+  data=[
+    Variable('x', int),
+    Variable('y', int),
+    Variable('actingID', int),
+  ],
+)
+
+drop = Animation('drop',
+  data=[
+    Variable('x', int),
+    Variable('y', int),
+    Variable('actingID', int),
+  ],
+)
+
+death = Animation('death',
+  data=[
+    Variable('actingID', int)
+  ],
+)
+
+
 
 
