a1c487fb56b3ba0023dab91f5acaf2badb2e03ac
bm4g5_000 <bryce@ubuntu.(none)>
2013-02-16 15:08:47 -0600

    * Initial visualizer plugin * Got it to compile with gcc 4.7+

---
+++ visualizer/reefAnimatable.h
@@ -0,0 +1,18 @@
+#ifndef REEF_ANIMATABLE_H
+#define REEF_ANIMATABLE_H
+
+//#include "reefAnimatable.h"
+#include "irenderer.h"
+#include "parser/structures.h"
+
+#include "math.h"
+
+namespace visualizer
+{
+    struct Something: public Animatable
+    {
+    };
+
+} // visualizer
+
+#endif // REEF_ANIMATABLE_H
