c5ab1061107903c743fb6d76ef07f3f9ac74fd03
bm4g5_000 <bm4g5@mst.edu>
2013-03-03 22:11:29 -0600

    Adding the resource file

--- plugins/reef/resources.r
+++ plugins/reef/resources.r
@@ -1,6 +1,4 @@
 font DroidSansMono textures/fonts/DroidSansMono.png DroidSansMono.dat
 font Roboto textures/fonts/Roboto.png Roboto.dat
-texture texture textures/texture.png
-texture coral1 textures/coral1.png
-texture coral2 textures/coral2.png
-texture coral3 textures/coral3.png
+texture water textures/tile_water.png
+animation coral textures/coral.png
