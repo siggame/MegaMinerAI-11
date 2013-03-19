f05b3cb9490a52e078b4d3da90e92755e832c636
Ethan <easvp4@rc08ucs213.managed.mst.edu>
2013-03-19 16:50:12 -0500

    my test ai

---
+++ testClients/simpson/Species.cpp
@@ -0,0 +1,83 @@
+// -*-c++-*-
+
+#include "Species.h"
+#include "game.h"
+
+
+Species::Species(_Species* pointer)
+{
+    ptr = (void*) pointer;
+}
+
+int Species::id()
+{
+  return ((_Species*)ptr)->id;
+}
+
+char* Species::name()
+{
+  return ((_Species*)ptr)->name;
+}
+
+int Species::cost()
+{
+  return ((_Species*)ptr)->cost;
+}
+
+int Species::maxHealth()
+{
+  return ((_Species*)ptr)->maxHealth;
+}
+
+int Species::maxMovement()
+{
+  return ((_Species*)ptr)->maxMovement;
+}
+
+int Species::carryCap()
+{
+  return ((_Species*)ptr)->carryCap;
+}
+
+int Species::attackPower()
+{
+  return ((_Species*)ptr)->attackPower;
+}
+
+int Species::range()
+{
+  return ((_Species*)ptr)->range;
+}
+
+int Species::maxAttacks()
+{
+  return ((_Species*)ptr)->maxAttacks;
+}
+
+int Species::season()
+{
+  return ((_Species*)ptr)->season;
+}
+
+
+bool Species::spawn(int x, int y)
+{
+  return speciesSpawn( (_Species*)ptr, x, y);
+}
+
+
+
+std::ostream& operator<<(std::ostream& stream,Species ob)
+{
+  stream << "id: " << ((_Species*)ob.ptr)->id  <<'\n';
+  stream << "name: " << ((_Species*)ob.ptr)->name  <<'\n';
+  stream << "cost: " << ((_Species*)ob.ptr)->cost  <<'\n';
+  stream << "maxHealth: " << ((_Species*)ob.ptr)->maxHealth  <<'\n';
+  stream << "maxMovement: " << ((_Species*)ob.ptr)->maxMovement  <<'\n';
+  stream << "carryCap: " << ((_Species*)ob.ptr)->carryCap  <<'\n';
+  stream << "attackPower: " << ((_Species*)ob.ptr)->attackPower  <<'\n';
+  stream << "range: " << ((_Species*)ob.ptr)->range  <<'\n';
+  stream << "maxAttacks: " << ((_Species*)ob.ptr)->maxAttacks  <<'\n';
+  stream << "season: " << ((_Species*)ob.ptr)->season  <<'\n';
+  return stream;
+}
---
+++ testClients/simpson/Species.h
@@ -0,0 +1,49 @@
+// -*-c++-*-
+
+#ifndef SPECIES_H
+#define SPECIES_H
+
+#include <iostream>
+#include "structures.h"
+
+
+///This class describes the characteristics for each type of fish. A groundbased fish is damaged each time it ends a turn above the groundBound Y value. Also, a species will only be available For so long, and new species will become available as a match progreses. 
+class Species {
+  public:
+  void* ptr;
+  Species(_Species* ptr = NULL);
+
+  // Accessors
+  ///Unique Identifier
+  int id();
+  ///The name of this species
+  char* name();
+  ///The amount of food it takes to raise this fish
+  int cost();
+  ///The maximum health of this fish
+  int maxHealth();
+  ///The maximum number of movements in a turn
+  int maxMovement();
+  ///The total weight the fish can carry
+  int carryCap();
+  ///The power of the fish's attack
+  int attackPower();
+  ///The attack arrange of the fish
+  int range();
+  ///Maximum number of times this unit can attack per turn
+  int maxAttacks();
+  ///Determines what season this species will be spawnable in
+  int season();
+
+  // Actions
+  ///Have a new fish spawn and join the fight!
+  bool spawn(int x, int y);
+
+  // Properties
+
+
+  friend std::ostream& operator<<(std::ostream& stream, Species ob);
+};
+
+#endif
+
