4c0e4f9e055803a7a6db4e88a8df68ef2f2bd782
AnthonyLonigro <ajl9t5@mail.mst.edu>
2013-04-01 12:39:23 -0500

    added a test client

---
+++ testClients/lonigro/AI.cpp
@@ -0,0 +1,30 @@
+#include "AI.h"
+#include "util.h"
+
+AI::AI(Connection* conn) : BaseAI(conn) {}
+
+enum AI::speciesIndex { SEA_STAR, SPONGE, ANGELFISH, CONESHELL_SNAIL, SEA_URCHIN, OCTOPUS, TOMCOD, REEF_SHARK, CUTTLEFISH, CLEANER_SHRIMP, ELECTRIC_EEL, JELLYFISH };
+
+
+const char* AI::username()
+{
+  return "Shell AI";
+}
+
+const char* AI::password()
+{
+  return "password";
+}
+
+//This function is run once, before your first turn.
+void AI::init(){}
+
+//This function is called each time it is your turn.
+//Return true to end your turn, return false to ask the server for updated information.
+bool AI::run()
+{
+  return true;
+}
+
+//This function is run once, after your last turn.
+void AI::end(){}
---
+++ testClients/lonigro/Fish.h
@@ -0,0 +1,68 @@
+// -*-c++-*-
+
+#ifndef FISH_H
+#define FISH_H
+
+#include <iostream>
+#include "structures.h"
+
+#include "Mappable.h"
+class Fish;
+
+///This is your primary unit for Reef. It will perform all of your major actions (pickup, attack, move, drop). It stats are based off of its species
+class Fish : public Mappable {
+  public:
+  Fish(_Fish* ptr = NULL);
+
+  // Accessors
+  ///Unique Identifier
+  int id();
+  ///X position of the object
+  int x();
+  ///Y position of the object
+  int y();
+  ///The owner of this fish
+  int owner();
+  ///The maximum health of the fish
+  int maxHealth();
+  ///The current health of the fish
+  int currentHealth();
+  ///The maximum number of movements in a turn
+  int maxMovement();
+  ///The number of movements left
+  int movementLeft();
+  ///The total weight the fish can carry
+  int carryCap();
+  ///The current amount of weight the fish is carrying
+  int carryingWeight();
+  ///The power of the fish's attack
+  int attackPower();
+  ///The visibleness of the fish
+  int isVisible();
+  ///The maximum number of attacks this fish has per turn
+  int maxAttacks();
+  ///The number of attacks a fish has left
+  int attacksLeft();
+  ///The attack range of the fish
+  int range();
+  ///The index of the fish species.
+  int species();
+
+  // Actions
+  ///Command a fish to move to a specified position
+  bool move(int x, int y);
+  ///Command a fish to pick up some trash at a specified position
+  bool pickUp(int x, int y, int weight);
+  ///Command a fish to drop some trash at a specified position
+  bool drop(int x, int y, int weight);
+  ///Command a fish to attack a target
+  bool attack(Fish& target);
+
+  // Properties
+
+
+  friend std::ostream& operator<<(std::ostream& stream, Fish ob);
+};
+
+#endif
+
