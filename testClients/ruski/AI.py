2b3a1d7173b293a394c769e82d394766bd38ba36
Russley Shaw <rfswf5@mst.edu>
2013-04-05 15:18:41 -0500

    Updated AI (a little)

--- testClients/ruski/AI.py
+++ testClients/ruski/AI.py
@@ -6,35 +6,72 @@ import time
 import math
 import os
 
+SEA_STAR, SPONGE, ANGELFISH, CONESHELL_SNAIL, SEA_URCHIN, OCTOPUS, TOMCOD, REEF_SHARK, CUTTLEFISH, CLEANER_SHRIMP, ELECTRIC_EEL, JELLYFISH = range(12)
 
 class AI(BaseAI):
   """The class implementing gameplay logic."""
 
-  coves = []
+  ########## LOCAL LISTS ##########
+  myCoves = []
+  enemyCoves = []
   myFish = []
   enemyFish = []
-  myTrash = []
+  trash = []
   charGrid = [[]]
   gridHistory = []
 
+  ########## USERNAME AND PASSWORD ##########
   @staticmethod
   def username():
     return "ruski"
-
   @staticmethod
   def password():
     return "password"
 
-  ##This function is called once, before your first turn
+  ########## START AND END ##########
   def init(self):
     print "Init"
+    self.getMyCoves()
+    self.getEnemyCoves()
     return
-
-  ##This function is called once, after your last turn
   def end(self):
     print "The End"
     self.replayGrid()
     return
+
+  ########## LOCAL GETTERS ##########
+  def getSpecies(self, index):
+    for spec in self.species:
+      if spec.index is index:
+        return spec
+    return "Invalid Index"
+  def getMyCoves(self):
+    self.myCoves = []
+    for tile in self.tiles:
+      if tile.owner is self.playerID:
+        self.myCoves.append(tile)
+    return
+  def getEnemyCoves(self):
+    self.enemyCoves = []
+    for tile in self.tiles:
+      if tile.owner is self.playerID^1:
+        self.enemyCoves.append(tile)
+    return
+  def getMyFish(self):
+    self.myFish = []
+    for fish in self.fishes:
+      if fish.owner is self.playerID:
+        self.myFish.append(fish)
+  def getEnemyFish(self):
+    for fish in self.fishes:
+      if fish.owner is self.playerID^1:
+        self.enemyFish.append(fish)
+  def getTrash(self):
+    self.trash = []
+    for tile in self.tiles:
+      if tile.trashAmount > 0:
+        self.trash.append(tile)
+
 
   ########## CHARACTER GRID ##########
   def getCharGrid(self):
@@ -70,7 +107,6 @@ class AI(BaseAI):
           print "|",
         elif x == self.mapWidth/2 + self.boundLength:
           print "|",
-
         print grid[x][y],
       print
     return
@@ -81,17 +117,73 @@ class AI(BaseAI):
   def taxiDist(self, x1, y1, x2, y2):
     return abs(x2-x1) + abs(y2-y1)
 
-  ##This function is called each time it is your turn
-  ##Return true to end your turn, return false to ask the server for updated information
-  def run(self):
-    print "Width: %i Height %i" % (self.mapWidth, self.mapHeight)
-    print "Starting Turn #%i P1: %i P2: %i" % (self.turnNumber, self.players[0].currentReefHealth, self.players[1].currentReefHealth)
+  ########## FISH MOVE FUNCTIONS ##########
+  def moveTo(self, fish, tx, ty):
+    for _ in fish.maxMovement:
+      #Move Right
+      if fish.x < tx:
+        if not self.attemptMove(fish, fish.x+1, fish.y):
+          if not self.attemptMove(fish, fish.x, fish.y+1):
+            self.attemptMove(fish, fish.x, fish.y-1)
+      #Move Left
+      elif fish.x > tx:
+        if not self.attemptMove(fish, fish.x-1, fish.y):
+          if not self.attemptMove(fish, fish.x, fish.y+1):
+            self.attemptMove(fish, fish.x, fish.y-1)
+      #Move Up
+      elif fish.y < ty:
+        self.attemptMove(fish, fish.x, fish.y+1)
 
-    #PRINT GRID
+      #Move Down
+      elif fish.y > ty:
+        self.attemptMove(fish, fish.x, fish.y-1)
+
+    return
+
+  def attemptMove(self, fish, tx, ty):
+    if 0 <= tx < self.mapWidth:
+      return False
+    if 0 <= ty < self.mapHeight:
+      return False
+
+    T = self.tiles[ty*self.mapWidth + tx]
+    print "Expected (%i, %i) Retrieved (%i, %i)" % (tx, ty, T.x, T.y)
+    if T.trashAmount > 0:
+      return False
+    if T.owner is self.playerID or 2:
+      return False
+    if T.hasEgg:
+      return False
+    if self.taxiDist(fish.x, fish.y, tx, ty) is not 1:
+      return False
+
+    for otherfish in self.fishes:
+      if otherfish.x == tx and otherfish.y == ty and otherfish.id != fish.id:
+        return False
+    return fish.move(tx, ty)
+
+  ########## DECIDE WHAT TO SPAWN ##########
+  def doSpawning(self):
+    spec = self.getSpecies(JELLYFISH)
+    me = self.players[self.playerID]
+    for cove in self.myCoves:
+      if me.spawnFood < spec.cost:
+        return
+      spec.spawn(cove.x, cove.y)
+    return
+
+  ########## RUN ##########
+  def run(self):
+    print "Starting Turn #%i P1: %i P2: %i" % (self.turnNumber, self.players[0].currentReefHealth, self.players[1].currentReefHealth)
+    self.getMyFish()
+    self.getEnemyFish()
+    self.getTrash()
+
     self.getCharGrid()
     self.gridHistory.append(self.charGrid)
 
-    #PRINT GRID
+    self.doSpawning()
+
     self.getCharGrid()
     self.gridHistory.append(self.charGrid)
 
