c68c76336b98af9c1f4c980947331f22d1fabb4b
Adam Harter <athb79@mail.mst.edu>
2013-04-05 15:21:16 -0500

    Shell AI junk.  Invisible cleaning shrimp.

---
+++ testClients/shellCPP/AI.h
@@ -0,0 +1,22 @@
+#ifndef AI_H
+#define AI_H
+
+#include "BaseAI.h"
+
+///The class implementing gameplay logic.
+class AI: public BaseAI
+{
+public:
+  AI(Connection* c);
+  virtual const char* username();
+  virtual const char* password();
+  virtual void init();
+  virtual bool run();
+  virtual void end();
+
+  Tile& getTile(int x,int y);
+  int getSpecies(int SpeciesNum);
+  Fish* getFish(int x,int y);
+};
+
+#endif
---
+++ testClients/shellCPP/BaseAI.cpp
@@ -0,0 +1,110 @@
+//Copyright (C) 2009 - Missouri S&T ACM AI Team
+//Please do not modify this file while building your AI
+//See AI.h & AI.cpp for that
+
+#include "BaseAI.h"
+#include "game.h"
+
+int BaseAI::maxReefHealth()
+{
+  return getMaxReefHealth(c);
+}
+int BaseAI::boundLength()
+{
+  return getBoundLength(c);
+}
+int BaseAI::turnNumber()
+{
+  return getTurnNumber(c);
+}
+int BaseAI::playerID()
+{
+  return getPlayerID(c);
+}
+int BaseAI::gameNumber()
+{
+  return getGameNumber(c);
+}
+int BaseAI::trashDamage()
+{
+  return getTrashDamage(c);
+}
+int BaseAI::mapWidth()
+{
+  return getMapWidth(c);
+}
+int BaseAI::mapHeight()
+{
+  return getMapHeight(c);
+}
+int BaseAI::trashAmount()
+{
+  return getTrashAmount(c);
+}
+int BaseAI::currentSeason()
+{
+  return getCurrentSeason(c);
+}
+int BaseAI::seasonLength()
+{
+  return getSeasonLength(c);
+}
+int BaseAI::healPercent()
+{
+  return getHealPercent(c);
+}
+
+bool BaseAI::startTurn()
+{
+  static bool initialized = false;
+  int count = 0;
+  count = getMappableCount(c);
+  mappables.clear();
+  mappables.resize(count);
+  for(int i = 0; i < count; i++)
+  {
+    mappables[i] = Mappable(getMappable(c, i));
+  }
+
+  count = getTileCount(c);
+  tiles.clear();
+  tiles.resize(count);
+  for(int i = 0; i < count; i++)
+  {
+    tiles[i] = Tile(getTile(c, i));
+  }
+
+  count = getSpeciesCount(c);
+  species.clear();
+  species.resize(count);
+  for(int i = 0; i < count; i++)
+  {
+    species[i] = Species(getSpecies(c, i));
+  }
+
+  count = getFishCount(c);
+  fishes.clear();
+  fishes.resize(count);
+  for(int i = 0; i < count; i++)
+  {
+    fishes[i] = Fish(getFish(c, i));
+  }
+
+  count = getPlayerCount(c);
+  players.clear();
+  players.resize(count);
+  for(int i = 0; i < count; i++)
+  {
+    players[i] = Player(getPlayer(c, i));
+  }
+
+  if(!initialized)
+  {
+    initialized = true;
+    init();
+  }
+  return run();
+}
+
+BaseAI::BaseAI(Connection* conn) : c(conn) {}
+BaseAI::~BaseAI() {}
