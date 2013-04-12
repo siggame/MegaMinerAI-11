e19026c4a027f03ab5aaed7fbb4e92d7d3637372
easvp4 <easvp4@mst.edu>
2013-04-12 17:10:57 -0500

    now my fish pickup and drop!

--- testClients/simpson/AI.cpp
+++ testClients/simpson/AI.cpp
@@ -35,9 +35,8 @@ Fish& AI::myGetFish(const int targetX, const int targetY)
   }
 }
 
-bool AI::isValidLoc(const int x,const int y,bool & isFish)
+bool AI::isValidLoc(const int x,const int y)
 {
-  isFish = false;
   if(myGetTile(x,y).hasEgg()) //invalid if egg
   {
     return false;
@@ -46,7 +45,6 @@ bool AI::isValidLoc(const int x,const int y,bool & isFish)
   {
     if(fishes[j].x() == x && fishes[j].y() == y) //invalid if fish
     {
-      isFish = true;
       return false;
     }
   }
@@ -61,12 +59,41 @@ bool AI::isValidLoc(const int x,const int y,bool & isFish)
   return true;
 }
 
+bool AI::isValidLoc(const int x,const int y,bool & isFish,bool & isTrash,int & trash)
+{
+  isFish = false;
+  isTrash = false;
+  trash = 0;
+  if(myGetTile(x,y).hasEgg()) //invalid if egg
+  {
+    return false;
+  }
+  for(int j = 0; j < fishes.size(); j++)
+  {
+    if(fishes[j].x() == x && fishes[j].y() == y) //invalid if fish
+    {
+      isFish = true;
+      return false;
+    }
+  }
+  if(myGetTile(x,y).trashAmount() > 0) //invalid if trash
+  {
+    isTrash = true;
+    trash = myGetTile(x,y).trashAmount();
+    return false;
+  }
+  if(myGetTile(x,y).owner() != 2) //invalid if cove or wall
+  {
+    return false;
+  }
+  return true;
+}
+
 bool AI::findPath(const int beginX,const int beginY,const int endX,const int endY,
               Fish & f)
 {
   int currentX = beginX;
   int currentY = beginY;
-  bool hasFish;
   for(int i = 0; i < 4; i++) //4 choices
   {
     switch(i)
@@ -87,7 +114,7 @@ bool AI::findPath(const int beginX,const int beginY,const int endX,const int end
         currentX--;
         break;
     }
-    if(isValidLoc(currentX,currentY,hasFish))
+    if(isValidLoc(currentX,currentY))
     {
       if(currentX == endX && currentY == endY) //success!
       {
@@ -108,8 +135,8 @@ bool AI::findPath(const int beginX,const int beginY,const int endX,const int end
 //Return true to end your turn, return false to ask the server for updated information.
 bool AI::run()
 {
-	int myX = -1,myY = -1,enemyX = -1, enemyY = -1, count = 0;
-  bool bob,hasFish;
+	int myX = -1,myY = -1,enemyX = -1, enemyY = -1, count = 0, numTrash;
+  bool bob,hasFish,hasTrash,karl = false;
   
 	for(int i = 0; i < tiles.size(); i++) //spawn fish
 	{
@@ -135,25 +162,44 @@ bool AI::run()
     {
       while(fishes[i].movementLeft() > 0)
       {
-        if(playerID() == 0)
-          bob = true;
-        else
-          bob = false;
+        playerID() == 0 ? bob = true : bob = false;
           
         //move right if player 1 else move left
         if(bob)
         {
-          if( isValidLoc(fishes[i].x()+1,fishes[i].y(),hasFish) )
+          if( isValidLoc(fishes[i].x()+1,fishes[i].y(),hasFish,hasTrash,numTrash) )
             fishes[i].move(fishes[i].x()+1,fishes[i].y());
           else if(hasFish) //attack if fish
             fishes[i].attack(myGetFish(fishes[i].x()+1,fishes[i].y()));
+          else if(hasTrash) //pickup if trash
+          {
+            if(numTrash > fishes[i].carryCap())
+            {
+              numTrash = fishes[i].carryCap();
+              karl = true;
+            }
+            fishes[i].pickUp(myGetTile(fishes[i].x()+1,fishes[i].y()), numTrash);
+            if(karl) //drop behind to keep moving
+              fishes[i].drop(myGetTile(fishes[i].x()-1,fishes[i].y()), numTrash);
+          }
         }
         else
         {
-          if( isValidLoc(fishes[i].x()-1,fishes[i].y(),hasFish) )
+          if( isValidLoc(fishes[i].x()-1,fishes[i].y(),hasFish,hasTrash,numTrash) ) 
             fishes[i].move(fishes[i].x()-1,fishes[i].y());
           else if(hasFish) //attack if fish
             fishes[i].attack(myGetFish(fishes[i].x()-1,fishes[i].y()));
+          else if(hasTrash) //pickup if trash
+          {
+            if(numTrash > fishes[i].carryCap())
+            {
+              numTrash = fishes[i].carryCap();
+              karl = true;
+            }
+            fishes[i].pickUp(myGetTile(fishes[i].x()-1,fishes[i].y()), numTrash);
+            if(karl) //drop behind to keep moving
+              fishes[i].drop(myGetTile(fishes[i].x()+1,fishes[i].y()), numTrash);
+          }
         }
       }
     }
