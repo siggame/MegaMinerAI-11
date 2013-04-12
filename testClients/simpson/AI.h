072de60b6f771e45436c28374037470c59e1e9be
easvp4 <easvp4@mst.edu>
2013-04-12 12:59:44 -0500

    an attempt to test fish attack function

--- testClients/simpson/AI.h
+++ testClients/simpson/AI.h
@@ -13,6 +13,10 @@ public:
   virtual void init();
   virtual bool run();
   virtual void end();
+  Tile myGetTile(const int x, const int y);
+  Fish myGetFish(const int targetX, const int targetY);
+  bool findPath(const int beginX, const int beginY,const int endX,const int endY,
+              Fish & f);
 };
 
 #endif
