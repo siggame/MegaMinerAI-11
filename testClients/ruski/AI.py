2a0c1b40e7f778d0f1a821e13e869fc884418f07
Russley Shaw <rfswf5@mst.edu>
2013-04-13 01:45:47 -0500

    Changed species config to suit Species.xlsx Also edited my AI. Beginning pathfinding.

--- testClients/ruski/AI.py
+++ testClients/ruski/AI.py
@@ -3,6 +3,7 @@ from BaseAI import BaseAI
 from GameObject import *
 
 import math
+import time
 
 SEA_STAR, SPONGE, ANGELFISH, CONESHELL_SNAIL, SEA_URCHIN, OCTOPUS, TOMCOD, REEF_SHARK, CUTTLEFISH, CLEANER_SHRIMP, ELECTRIC_EEL, JELLYFISH = range(12)
 
@@ -13,6 +14,8 @@ class AI(BaseAI):
   myFish = []
   trash = []
 
+  gridHistory = []
+
   @staticmethod
   def username():
     return "ruski"
@@ -20,6 +23,43 @@ class AI(BaseAI):
   @staticmethod
   def password():
     return "password"
+
+########## DO GRID STUFF ##########
+  def saveGrid(self):
+    tempGrid = [[' ' for _ in range(self.mapWidth) ] for _ in range(self.mapHeight) ]
+    for tile in self.tiles:
+      if tile.owner == 0 or tile.owner == 1:
+        tempGrid[tile.y][tile.x] = 'C'
+      if tile.owner == 3:
+        tempGrid[tile.y][tile.x] = 'W'
+      if tile.trashAmount > 0:
+        tempGrid[tile.y][tile.x] = 'T'
+      if tile.hasEgg == 1:
+        tempGrid[tile.y][tile.x] = 'e'
+
+    for fish in self.fishes:
+      if fish.owner == self.playerID:
+        tempGrid[fish.y][fish.x] = 'F'
+      else:
+        tempGrid[fish.y][fish.x] = 'f'
+
+    self.gridHistory.append(tempGrid)
+    return
+
+  def replayHistory(self):
+    for grid in self.gridHistory:
+      self.printGrid(grid)
+      time.sleep(.1)
+    return
+
+  def printGrid(self, grid):
+    print "--" * self.mapWidth
+    for columns in grid:
+      for data in columns:
+        print data,
+      print
+    return
+
 
 ########## GET LOCAL LISTS ##########
   def getMyCoves(self):
@@ -45,9 +85,12 @@ class AI(BaseAI):
             species.spawn(cove)
             break
     return
+  def generatePath(self, sourceX, sourceY, targetX, targetY):
+
+    return
 
   def actFish(self):
-    for fish in self.myFish:
+    #for fish in self.myFish:
 
     return
 
@@ -65,16 +108,27 @@ class AI(BaseAI):
 
   def end(self):
     print "End"
+
+    print "Replaying Grid in ..."
+    time.sleep(1)
+    print "3 ..."
+    time.sleep(1)
+    print "2 ..."
+    time.sleep(1)
+    print "1 ..."
+    time.sleep(1)
+    self.replayHistory()
+
     return
 
-  ##This function is called each time it is your turn
-  ##Return true to end your turn, return false to ask the server for updated information
   def run(self):
-    print "Starting Turn %i" % self.turnNumber
-    self.spawnFish()
+    print "Turn %i P1: %i P2 %i" % (self.turnNumber, self.players[0].currentReefHealth, self.players[1].currentReefHealth)
+    self.saveGrid()
 
+    self.spawnFish()
     self.actFish()
 
+    self.saveGrid()
     return 1
 
   def __init__(self, conn):
